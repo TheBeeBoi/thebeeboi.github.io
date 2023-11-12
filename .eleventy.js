02d9976decc16070346a363c89209b549f6c239d
TheBeeBoi <architgis10@gmail.com>
2023-11-12 11:55:29 +0530

    Feeds! RSS, Atom, and JSON!

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
 module.exports = function (config) {
     config.addPassthroughCopy("./src/style.css");
+    config.addPassthroughCopy("./src/xml.css");
     config.addPassthroughCopy("./src/script.js");
     config.addPassthroughCopy("./src/extras", {});
     config.addPassthroughCopy("./src/favicon.ico");
     config.addPassthroughCopy("./jetbra.in_codes");
+    config.addPlugin(require("@11ty/eleventy-plugin-rss"));
     return {
         dir: {
             input: "src",
